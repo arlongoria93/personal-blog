fd6c1192b47bec2e7d3c27dd51444cd92aff27f6
Angelo Longoria <42070067+arlongoria93@users.noreply.github.com>
2022-06-22 12:12:03 -0500

    creating project schema for sanity

---
+++ personal-blog/schemas/post.js
@@ -0,0 +1,21 @@
+export default {
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    { name: "title", title: "Title", type: "string" },
+    { name: "stack", title: "Stack", type: "string" },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 200,
+        slugify: (input) =>
+          input.toLowerCase().replace(/\s+/g, "-").slice(0, 200),
+      },
+    },
+    { name: "description", title: "Description", type: "text" },
+  ],
+};
