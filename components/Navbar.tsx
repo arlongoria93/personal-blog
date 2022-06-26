befc2d5bff8551a7dec6f1177f02a4573d457083
Angelo Longoria <42070067+arlongoria93@users.noreply.github.com>
2022-06-26 10:47:52 -0500

    working on right content section of main page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,30 @@
+import Link from "next/link";
+
 const Navbar = () => {
-  return <nav className="navbar">Angelo</nav>;
+  return (
+    <div className="navbar bg-base-100 max-w-7xl border border-yellow-500 mx-auto">
+      <div className="flex-1">
+        <Link href="/">
+          <a className="btn btn-ghost normal-case text-xl">
+            <span className="text-primary">~</span>
+            <span className="text-accent ">{`${"["}`}</span>
+            angelo<span className="text-error">@</span>longoria
+            <span className="text-accent ">{`${"]"}`}</span>
+          </a>
+        </Link>
+        <ul tabIndex={4} className="menu menu-horizontal p-0">
+          <li>
+            <Link href="/projects">
+              <a>Projects</a>
+            </Link>
+          </li>
+          <li>
+            <a>Resume</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import Project from "../components/Project";
 import { getClient } from "../lib/sanity.server";
@@ -25,28 +24,47 @@ export const getServerSideProps = async () => {
 interface Props {
   projects: Projects[];
 }
-
 const Home = ({ projects }: Props) => {
-  console.log(projects);
   return (
     <div className="max-w-7xl mx-auto border border-yellow-200">
       <Head>
-        <title>Angelo Longoria</title>
+        <title>angelo@longoria</title>
         <meta
           name="description"
           content="Angelo Longoria personal website and blog"
         />
         <link rel="icon" href="/sword-in-stone.png" />
       </Head>
+
+      {/* Left content section */}
       <div className="flex justify-between">
         <div className="max-w-lg items-center p-4 justify-center border border-red-400 flex flex-col">
-          <p>Angelo Longoria</p>
+          <p>Left Content Section</p>
         </div>
-        <div className="max-w-2xl flex flex-col items-center  space-y-12 border border-green-300">
-          {projects.map((project) => (
-            <Project {...project} />
-          ))}
+
+        {/* Right content section */}
+        <div>
+          <div className="w-full flex flex-col justify-start border border-green-700 prose ">
+            <h1>Social</h1>
+            <ul className="list-none flex flex-col justify-start items-center p-0 text-left border border-yellow-200">
+              <li>
+                <h3>Github</h3>
+              </li>
+              <li>
+                <h3>LinkedIn</h3>
+              </li>
+              <li>
+                <h3>Twitter</h3>
+              </li>
+            </ul>
+          </div>
+          {/* <div className="max-w-2xl w-full flex flex-col items-center  space-y-12 border border-green-300">
+            {projects.map((project) => (
+              <Project {...project} />
+            ))}
+          </div> */}
         </div>
+        {/* End of Right */}
       </div>
     </div>
   );
